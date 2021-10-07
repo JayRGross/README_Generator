7411b5ebd73fb93076ec84f2c152d8632e9ae340
Jaylin Gross <grossjaylin@gmail.com>
2021-10-07 19:06:35 -0400

    added index,js code

--- index.js
+++ index.js
@@ -1,15 +1,66 @@
-var inquirer = require('inquirer');
-inquirer
-  .prompt([
-    /* Pass your questions in here */
-  ])
-  .then((answers) => {
-    // Use user feedback for... whatever!!
-  })
-  .catch((error) => {
-    if (error.isTtyError) {
-      // Prompt couldn't be rendered in the current environment
-    } else {
-      // Something else went wrong
-    }
-  });
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require('./util/generateMarkdown');
+
+const promptUser = () => {
+    return inquirer.prompt([
+        {
+        type: 'input',
+        name: 'Title',
+        message: 'Whats the project title?'
+        },
+        {
+        type: 'input',
+        name: 'Description',
+        message: 'Describe your project?'
+        },
+        {
+        type: 'input',
+        name: 'Installation',
+        message: 'What packages did you install?'    
+        },
+        {
+        type: 'input',
+        name: 'Usage',
+        message: 'What are some usage examples?'
+        },
+        {
+        type: 'input',
+        name: 'Contributing',
+        message: 'How did you contribute to your project?'    
+        },
+        {
+        type: 'input',
+        name: 'Test',
+        message: 'How do you test?'    
+        },
+        {
+        type: 'input',
+        name: 'Email',
+        message: 'Enter your email address'    
+        },
+        {
+        type: 'input',
+        name: 'Github',
+        message: 'What is your github username?'    
+        },
+        {
+        type: 'list',
+        name: 'License',
+        message: 'choose a license for your README.',
+        choices: [
+            'Apache2', 
+            'GNUGPv3',
+            'IBM',
+            'BSD',
+            'MIT',
+            'Mozilla',
+        ]
+    }    
+    ])
+};
+const generateReadMe = () => {
+    promptUser()
+    .then((answers) => fs.writeFile('READ.md', generateMarkdown(answers), () => console.error('your file has been generated')))
+    .catch((err) => {console.log(err)})
+};
