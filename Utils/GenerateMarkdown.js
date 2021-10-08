e8e4a811bc59fa36ea80467561aa32cabcabbb9e
Jaylin Gross <grossjaylin@gmail.com>
2021-10-07 22:44:27 -0400

    generate markdown edit

---
+++ Utils/GenerateMarkdown.js
@@ -0,0 +1,11 @@
+function renderLicenseSection() {}
+
+function renderLicenseBadge() {}
+
+function renderLicenseLink() {}
+
+function generateMarkdown(data) {
+    return `# ${data.title}`;
+}
+
+module.exports = generateMarkdown;
